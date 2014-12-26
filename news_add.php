a60695f68a2824f628ab315d767b326b4128998f
Danja Mewes <tealdeal@users.noreply.github.com>
2014-12-26 03:23:14 -0500

    Update news_add.php

--- news_add.php
+++ news_add.php
@@ -11,7 +11,7 @@
 echo "<p><h2>Date/Title: </h2><textarea name='dt' id='dt' style='width: 100%'></textarea><h2>Content</h2><textarea name='txt' id='txt' style='width: 100%'></textarea></p>";
 ?>
 
-<script type='text/javascript' src='nicedit/nicEdit.js'></script> 
+<script type='text/javascript' src='https://tealdeal.github.io/corral-your-xml/nicedit/nicEdit.js'></script> 
 <script type='text/javascript'>
 //<![CDATA[
 	bkLib.onDomLoaded(function() { nicEditors.allTextAreas({buttonList : ['fontSize','bold','italic','underline','strikeThrough','subscript','superscript','link','unlink','xhtml'], iconsPath : 'nicedit/nicEditorIcons.gif'})});
